d1014f9dcc6e7d4d6a7dafe7fc52c994f84a09b1
Kayque Coelho <lkcoelho76@gmail.com>
2021-11-05 16:52:35 -0300

    Clique parcialmente funcional

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+let contadorDeCartasViradas = 0;
+let cartasViradas = [];
+let filhosDasCartasViradas = []
+let paresVirados = 0;
 function configurações() {
     let númeroDeCartas = parseInt(prompt("Com quantas cartas você deseja jogar?"));
 
-    const endereços = ["/assets/explodyparrot.gif",
+    const endereçosDasCartas = ["/assets/explodyparrot.gif",
         "/assets/bobrossparrot.gif",
         "/assets/fiestaparrot.gif",
         "/assets/tripletsparrot.gif",
@@ -14,36 +18,87 @@ function configurações() {
         númeroDeCartas = parseInt(prompt("Com quantas cartas você deseja jogar?"))
     }
 
-    const númeroDeRepetições = númeroDeCartas/2;
+    const númeroDeRepetições = númeroDeCartas / 2;
     const cardsHTML = []
     let elementosHTML;
     for (let i = 0; i < númeroDeRepetições; i++) {
-        for (let j = 0; j < 2; j++){
-            elementosHTML = `<article class="card">
-            <div class="front-face face">
+        for (let j = 0; j < 2; j++) {
+            elementosHTML = `<article class="card" data-identifier="card">
+            <div class="front-face face" data-identifier="back-face" onclick="CliqueNaCarta(this)">
             <img src="/assets/front.png" >
             </div>
-            <div class="back-face face">
-            <img src="${endereços[i]}" >
+            <div class="back-face face" data-identifier="front-face">
+            <img src="${endereçosDasCartas[i]}" >
             </div>
             </article>
             `
             cardsHTML.push(elementosHTML)
         }
     }
-    
+
     cardsHTML.sort(comparador)
-    
+
     const seção = document.querySelector("section");
-    for (let i = 0; i < cardsHTML.length;i++ ){
+    for (let i = 0; i < cardsHTML.length; i++) {
         seção.innerHTML += cardsHTML[i]
     }
-    
-    
+
+}
+
+function CliqueNaCarta(endereçoClique) {
+    const card = endereçoClique.parentNode
+    const filhos = card.children
+
+    if (contadorDeCartasViradas < 2) {
+        cartasViradas.push(card)
+        filhosDasCartasViradas.push(filhos)
+        contadorDeCartasViradas++
+        for (let i = 0; i < filhos.length; i++) {
+            let filho = filhos[i];
+            let verificar = filhos[i].classList.contains("front-face")
+
+            if (verificar) {
+                filhos[i].classList.add("leite")
+            } else {
+                filhos[i].classList.add("docinho")
+            }
+        }
+    }
+
+    console.log(contadorDeCartasViradas)
+    console.log(cartasViradas)
+    console.log(filhosDasCartasViradas)
+    console.log("oi")
+    compararCartas()
+}
+function compararCartas() {
+    const carta1 = cartasViradas[0]
+    const carta2 = cartasViradas[1]
+
+    if (carta1.innerHTML === carta2.innerHTML) {
+        console.log("As cartas são iguais!")
+        paresVirados++
+    } else {
+        for (let j = 0; j < filhosDasCartasViradas; j++) {
+            for (let i = 0; i < filhos.length; i++) {
+                let filho = filhos[i];
+                let verificar = filhos[i].classList.contains("front-face")
+
+                if (verificar) {
+                    filhos[i].classList.add("leite")
+                } else {
+                    filhos[i].classList.add("docinho")
+                }
+            }
+        }
+
+    }
+}
 
 
+
+
+function comparador() {
+    return Math.random() - 0.5;
 }
 configurações()
-function comparador() { 
-	return Math.random() - 0.5; 
-}
