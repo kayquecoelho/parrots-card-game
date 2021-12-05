d2d4ba22020e513606c40129f391d8dc98375167
Kayque Coelho <lkcoelho76@gmail.com>
2021-12-05 17:09:19 -0300

    cleaning code

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ let contadorDeCartasSelecionadas = 0;
 let cartasViradas = 0;
 let númeroDeCartas;
 let qtdJogadas = 0
-let tempo = 0;
+let cronometro = 0;
 let id;
 
 function configurações() {
     númeroDeCartas = parseInt(prompt("Com quantas cartas você deseja jogar?", "Digite um número par entre 4 e 14."));
 
-    const endereçoDasImagens = ["/assets/explodyparrot.gif",
+    while (númeroDeCartas % 2 !== 0 || númeroDeCartas < 4 || númeroDeCartas > 14 || númeroDeCartas === NaN) {
+        númeroDeCartas = parseInt(prompt("Com quantas cartas você deseja jogar?"))
+    }
+    
+    const endereçoDasImagens = [
+        "/assets/explodyparrot.gif",
         "/assets/bobrossparrot.gif",
         "/assets/fiestaparrot.gif",
         "/assets/tripletsparrot.gif",
@@ -16,23 +21,22 @@ function configurações() {
         "/assets/revertitparrot.gif",
         "/assets/unicornparrot.gif"
     ]
-
-    while (númeroDeCartas % 2 !== 0 || númeroDeCartas < 4 || númeroDeCartas > 14 || númeroDeCartas === NaN) {
-        númeroDeCartas = parseInt(prompt("Com quantas cartas você deseja jogar?"))
-    }
+    endereçoDasImagens.sort(comparador)
 
     const númeroDeRepetições = númeroDeCartas / 2;
     const cardsHTML = []
     let elementosHTML;
+    
     for (let i = 0; i < númeroDeRepetições; i++) {
         for (let j = 0; j < 2; j++) {
-            elementosHTML = `<article class="card" data-identifier="card" onclick="recebeClique(this)">
-            <div class="front-face face" data-identifier="front-face" >
-            <img src="/assets/front.png" >
-            </div>
-            <div class="back-face face" data-identifier="back-face">
-            <img src="${endereçoDasImagens[i]}" >
-            </div>
+            elementosHTML = `
+            <article class="card" data-identifier="card" onclick="recebeClique(this)">
+                <div class="front-face face" data-identifier="front-face" >
+                <img src="/assets/front.png" >
+                </div>
+                <div class="back-face face" data-identifier="back-face">
+                <img src="${endereçoDasImagens[i]}" >
+                </div>
             </article>
             `
             cardsHTML.push(elementosHTML)
@@ -46,9 +50,9 @@ function configurações() {
         seção.innerHTML += cardsHTML[i]
     }
 
-    id = setInterval(contarTempo, 1000)
-
+    id = setInterval(cronometrar, 1000)
 }
+
 function recebeClique(endereçoClique) {
     const verificação = endereçoClique.classList.contains("virado")
     if (!verificação) {
@@ -58,25 +62,21 @@ function recebeClique(endereçoClique) {
     }
 }
 function CliqueNaCarta(endereçoClique) {
-
     const verificação = endereçoClique.classList.contains("selecionado")
 
     if (!verificação) {
         endereçoClique.classList.add("selecionado")
-        const frontFace = endereçoClique.querySelector(" .front-face")
-        const backFace = endereçoClique.querySelector(" .back-face")
-
+        const frontFace = endereçoClique.querySelector(".front-face")
+        const backFace = endereçoClique.querySelector(".back-face")
         frontFace.classList.add("face-frente")
         backFace.classList.add("face-costas")
 
         contadorDeCartasSelecionadas++
         qtdJogadas++
-
     }
 
     if (contadorDeCartasSelecionadas === 2) {
         compararCartas()
-
     }
 }
 function compararCartas() {
@@ -91,13 +91,11 @@ function compararCartas() {
 
         cartasViradas = cartasViradas + 2
         contadorDeCartasSelecionadas = 0
-
     } else {
         setTimeout(virarCartas, 1000)
     }
 
     setTimeout(fimDoJogo, 1000)
-
 }
 
 function virarCartas() {
@@ -110,23 +108,20 @@ function virarCartas() {
         frontFace.classList.remove("face-frente")
         backFace.classList.remove("face-costas")
         cartasSelecionadas[i].classList.remove("selecionado")
-
     }
 
-    contadorDeCartasSelecionadas = 0
-
+    contadorDeCartasSelecionadas = 0;
 }
 function fimDoJogo() {
     if (cartasViradas === númeroDeCartas) {
         clearInterval(id)
-        alert(`Você ganhou em ${qtdJogadas} jogadas e em ${tempo} segundos!  `)
+        alert(`Você ganhou em ${qtdJogadas} jogadas e em ${cronometro} segundos!  `)
         const pergunta = prompt("Você deseja jogar novamente?", "sim")
 
         if (pergunta === "sim") {
             document.location.reload(true)
         } else {
             alert("Obrigado por jogar!")
-
         }
     }
 }
@@ -135,10 +130,10 @@ function comparador() {
     return Math.random() - 0.5;
 }
 
-function contarTempo() {
-    tempo++
+function cronometrar() {
+    cronometro++;
     const contador = document.querySelector(".contador");
-    contador.innerHTML = tempo
+    contador.innerHTML = cronometro;
 }
 
 configurações()
